6974b114886a10793073239f93632b92383ec132
Jennifer Spriggs <jenniferlspriggs@gmail.com>
2015-05-28 20:35:00 -0400

    commiting index.html page changes

--- js/main.js
+++ js/main.js
@@ -4,4 +4,11 @@ $(document).ready(function(){
 	$('#nav').affix({
 	      offset: { top: $('#nav').offset().top }
 	});	
+
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top
+	    }, 700);
+	    return false;
+	});
 });
